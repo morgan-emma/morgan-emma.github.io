1b2b6cd321eba7c022182cede946688b29149c95
Emma Morgan <emmacm2@byui.edu>
2018-10-21 14:17:24 -0600

    Create topic8.js

---
+++ topic8.js
@@ -0,0 +1,9 @@
+var moon = document.querySelector(".animate")
+
+function moveMoon() {
+    moon.style.animationPlayState="running";  
+};
+
+function stopMoon() {
+    moon.style.animationPlayState="paused";  
+};
